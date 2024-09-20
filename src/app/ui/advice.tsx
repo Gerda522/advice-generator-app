de930a4aa7edbab547155df01a219a4c9eb35688
gnaConversio <gna@conversio.dk>
2024-09-20 10:55:11 +0200

    Update advice component

--- src/app/ui/advice.tsx
+++ src/app/ui/advice.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { getAdvice } from "../lib/actions";
 
 export function Advice() {
@@ -13,23 +13,25 @@ export function Advice() {
     setAdvice(result.slip);
   };
 
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-customDark">
       {advice && (
-        <div className="relative flex flex-col items-center text-lightCyan bg-customGray p-8 w-1/4 text-center gap-4">
+        <div className="relative flex flex-col items-center text-lightCyan bg-customGray p-8 pb-14 md:m-0 m-4 w-2/3 md:w-1/4 text-center gap-4 rounded-2xl">
           <p className="text-neonGreen">Advice # {advice.id}</p>
           <p>{advice.advice}</p>
           <img src="/pattern-divider-desktop.svg" alt="" />
+          <button
+            onClick={fetchAdvice}
+            className="absolute bottom-[0px] bg-neonGreen p-4 rounded-full transform translate-y-1/2"
+          >
+            <img src="/icon-dice.svg" className="text-black" alt="dice icon" />
+          </button>
         </div>
       )}
-
-      <button
-        onClick={fetchAdvice}
-        className="bg-neonGreen p-4 rounded-full m-4"
-      >
-        {" "}
-        <img src="/icon-dice.svg" className="text-black " alt=" dice icon" />
-      </button>
     </div>
   );
 }
